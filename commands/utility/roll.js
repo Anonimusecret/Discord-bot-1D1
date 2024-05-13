13b4f2fbd908bd766c8f599de06c17db97ee2260
Void <secretceeper25@gmail.com>
2024-05-13 23:16:39 +0300

    Улучшенные роллы с использованием RegEx и поддержкой сложных формул

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -15,17 +15,66 @@ module.exports = {
             await interaction.deferReply();
             let message = '\n`[';
             let sum = 0;
+            let pattern = /[\+|\-|*|\/]/; //регулярные выражения + - * и / 
+            let diceToken = /d/;
             const request = interaction.options.getString('request', true);
-            const quantity = request.substring(0, request.indexOf('d'));
+            let process = request;
+            let formula = '';
+            while (process.length > 0){
+                // понять есть ли до 2d20 что-то
+                let check = process.substring(0, process.indexOf(process.match(pattern)) + 1);
+                formula += process.substring(0, process.indexOf(process.match(pattern)) + 1);
+                if(diceToken.test(formula)){
+                    formula = formula.replace(process.substring(0, process.indexOf(process.match(pattern)) + 1), '');
+                } else {
+                    process = process.replace(formula , '');
+                }
+                
+                let quantity = process.substring(0, process.indexOf('d'));
+                let dice = process.substring(process.indexOf('d') + 1, process.indexOf(process.match(pattern)));
+                // тут роллим
+                for( let q = quantity ; q > 0; q-- ){ // бросаем каждый куб отдельно
+                    let roll = (Math.floor(Math.random() * dice)+1);
+                    
+                    if( q === quantity ){
+                        formula += roll
+                        message += roll
+                    } else {
+                        formula += '+' + roll
+                        message += ', ' + roll
+                    }
+                }
+                console.log('quantity = ' + quantity); 
+                console.log('dice = ' + dice);
+                console.log('formula = ' + formula);
+                //тут меняем
+                process = process.replace(process.substring(0, process.indexOf(dice) + 1) , '');
+                if (!diceToken.test(process)){
+                    formula += process;
+                    process = '';
+                }
+            }
+            try {
+                sum = eval(formula);
+            } catch (error) {
+                console.error(error);
+            }
+            message += ']` \nСуммарно = `' + sum + '`' // закрываем сообщение
+            await interaction.followUp(`${interaction.user.globalName} Роллит ` + '`' + request + '`' + message); // выводим результат
+
+            /*
+            let quantity = request.substring(0, request.indexOf('d'));
             let dice = request.substring(request.indexOf('d') + 1, request.indexOf('+'));
             let modif = request.substring(request.indexOf('+') + 1);
             let sumMod = 0;
+            
             if (request.indexOf('+') != -1){ // вытягиваем кость до модификатора
                 dice = request.substring(request.indexOf('d') + 1, request.indexOf('+'));
             } else { // или просто берем её если модификатора нет
                 dice = request.substring(request.indexOf('d') + 1);
             }
             
+
             
             if(quantity > 100){
                 await interaction.followUp('Простите, бросить больше 100 кубов нельзя');
@@ -33,7 +82,7 @@ module.exports = {
                 await interaction.followUp('Простите, бросить куб больше d1000 нельзя');
             } else {
 
-                for( let q = quantity ; q > 0; q-- ){ // бросаем каждый кб отдельно
+                for( let q = quantity ; q > 0; q-- ){ // бросаем каждый куб отдельно
                     let roll = (Math.floor(Math.random() * dice)+1);
                     
                     if( q === quantity ){
@@ -44,7 +93,8 @@ module.exports = {
                     sum += roll; // суммируем результат бросков
                 }
 
-                if (request.indexOf('+') != -1 ){ // если есть модификатор к броску то считаем его к сумме
+                
+                if (pattern.test(request) != -1 ){ // если есть модификатор к броску то считаем его к сумме
                     //let sumMod = 0; //обьявление суммы модификаторов тут, но для теста пока в комментарии и обьявлено выше
                     while( modif.indexOf('+') != -1 ){ // если модификатор не +x а +x+y+z+...+n
 
@@ -54,14 +104,15 @@ module.exports = {
                         //console.log(modif);
 
                     }
-                    console.log(request.indexOf('+'));
+                    //console.log(request.indexOf('+'));
                     sumMod += Number(modif); //добавляем  последнюю цифру в модификаторе ибо цикл её не считает
                     sum += sumMod;// прибавляем к сумме результатов броска итоговый модификатор
                 } else {
                     modif = 0;
-                }
+                
                 message += ']` \nСуммарно = `' + sum + '`' // закрываем сообщение
                 await interaction.followUp(`${interaction.user.globalName} Роллит ` + '`' + request + '`' + message + '\nМодификатор: ' + sumMod ); // выводим результат
-            }
+            
+            }}*/
         }
 };
