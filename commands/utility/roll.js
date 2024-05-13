2253b5d523d729b71ce310cdbe5cc1cc2ef53499
Void <secretceeper25@gmail.com>
2024-05-13 15:26:03 +0300

    Добавлена возможность прибавлять модификатор к броску

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -17,15 +17,23 @@ module.exports = {
             let sum = 0;
             const request = interaction.options.getString('request', true);
             const quantity = request.substring(0, request.indexOf('d'));
-            const dice = request.substring(request.indexOf('d') + 1);
-            const modif = request.substring(request.indexOf('+'));
+            let dice = request.substring(request.indexOf('d') + 1, request.indexOf('+'));
+            let modif = request.substring(request.indexOf('+') + 1);
+            let sumMod = 0;
+            if (request.indexOf('+') != -1){ // вытягиваем кость до модификатора
+                dice = request.substring(request.indexOf('d') + 1, request.indexOf('+'));
+            } else { // или просто берем её если модификатора нет
+                dice = request.substring(request.indexOf('d') + 1);
+            }
+            
+            
             if(quantity > 100){
                 await interaction.followUp('Простите, бросить больше 100 кубов нельзя');
             } else if (dice > 1000){
                 await interaction.followUp('Простите, бросить куб больше d1000 нельзя');
             } else {
 
-                for( let q = quantity ; q > 0; q-- ){
+                for( let q = quantity ; q > 0; q-- ){ // бросаем каждый кб отдельно
                     let roll = (Math.floor(Math.random() * dice)+1);
                     
                     if( q === quantity ){
@@ -33,12 +41,27 @@ module.exports = {
                     } else {
                         message += ', ' + roll
                     }
-                    sum += roll;
+                    sum += roll; // суммируем результат бросков
                 }
 
-                sum += modif;
-                message += ']` \nСуммарно = `' + sum + '`'
-                await interaction.followUp(`${interaction.user.globalName} Роллит ` + '`' + request + '`' + message );
+                if (request.indexOf('+') != -1 ){ // если есть модификатор к броску то считаем его к сумме
+                    //let sumMod = 0; //обьявление суммы модификаторов тут, но для теста пока в комментарии и обьявлено выше
+                    while( modif.indexOf('+') != -1 ){ // если модификатор не +x а +x+y+z+...+n
+
+                        sumMod += Number(modif.substring(0, modif.indexOf('+')));
+                        //console.log(sumMod);
+                        modif = modif.replace(modif.substring(0, modif.indexOf('+') + 1), '');
+                        //console.log(modif);
+
+                    }
+                    console.log(request.indexOf('+'));
+                    sumMod += Number(modif); //добавляем  последнюю цифру в модификаторе ибо цикл её не считает
+                    sum += sumMod;// прибавляем к сумме результатов броска итоговый модификатор
+                } else {
+                    modif = 0;
+                }
+                message += ']` \nСуммарно = `' + sum + '`' // закрываем сообщение
+                await interaction.followUp(`${interaction.user.globalName} Роллит ` + '`' + request + '`' + message + '\nМодификатор: ' + sumMod ); // выводим результат
             }
         }
 };
