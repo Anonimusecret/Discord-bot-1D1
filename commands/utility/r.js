636b39a552404150164c7d955823ae10bae7d1f5
Void <secretceeper25@gmail.com>
2024-05-16 19:36:39 +0300

    кнопка в roll теперь многоразовая, но выдаёт ошибку

--- commands/utility/r.js
+++ commands/utility/r.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, Events } = require('discord.js');
 
 module.exports = {
     cooldown: 0,
@@ -23,36 +23,74 @@ module.exports = {
                 components: [row],
             });
 
+            // Examples:
+                // Create a button interaction collector
+            // const filter = (interaction) => interaction.customId === 'button' && interaction.user.id === 'someId';
+            // const collector = channel.createMessageComponentCollector({ filter, time: 15_000 });
+            // collector.on('collect', interaction => console.log(`Collected ${interaction.customId}`));
+            // collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+
             const collectorFilter = i => i.user.id === interaction.user.id;
             let rollResult = '';
             let roll;
 
-            try {
-                const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+            const collector = response.createMessageComponentCollector({ filter: collectorFilter, time: 3_600_000 });
+
+            collector.on('collect', async i => {
+                //const selection = i.values[0];
 
                 roll = Math.floor(Math.random() * 20)+1
+
                 if (roll === 20){
                     rollResult = roll + ' **Критический успех** <:crit:1037660173659029505>'
                     await interaction.editReply({
                         content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
-                        components: [],
+                        components: [row],
                     });
                 } else if (roll === 1){
                     rollResult = roll + ' **Критический провал** <:Cubes:956132971725869076>'
                     await interaction.editReply({
                         content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
-                        components: [],
+                        components: [row],
                     });
                 } else {
                     rollResult = String(roll);
                     await interaction.editReply({
                         content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
-                        components: [],
+                        components: [row],
                     });
                 }
-            } catch (e) {
-                await interaction.editReply({ content: 'Кнопку не нажали', components: [] });
-            }
+                collector.on('end', async collected => await interaction.editReply(`Брошено ${collected.size} кубов`));
+                // await i.reply(`${i.user} has selected ${selection}!`);
+            });
+            
+
+            // try {
+            //\     const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+            //     roll = Math.floor(Math.random() * 20)+1
+            //     if (roll === 20){
+            //         rollResult = roll + ' **Критический успех** <:crit:1037660173659029505>'
+            //         await interaction.editReply({
+            //             content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
+            //             components: [],
+            //         });
+            //     } else if (roll === 1){
+            //         rollResult = roll + ' **Критический провал** <:Cubes:956132971725869076>'
+            //         await interaction.editReply({
+            //             content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
+            //             components: [],
+            //         });
+            //     } else {
+            //         rollResult = String(roll);
+            //         await interaction.editReply({
+            //             content: 'Нажмите на кнопу чтобы бросить 1d20 \nРезультат: ' + rollResult ,
+            //             components: [],
+            //         });
+            //     }
+            // } catch (e) {
+            //     await interaction.editReply({ content: 'Кнопку не нажали', components: [] });
+            // }
 
             
         }
