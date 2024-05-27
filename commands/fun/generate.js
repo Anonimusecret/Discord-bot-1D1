8a5621f1939a1fa9f6d534fa69d96ed17707e3cb
Void <secretceeper25@gmail.com>
2024-05-28 01:09:22 +0300

    команда /generate добавляю возможность рандомить поинт бай

--- commands/fun/generate.js
+++ commands/fun/generate.js
@@ -16,63 +16,88 @@ module.exports = {
         async execute(interaction) {	
             await interaction.deferReply({ephemeral: true});	
             
-            let maxPoints = interaction.options.getInteger('points', false);
-            let stats = rollStats();
+            let maxPoints = interaction.options.getInteger('points', false) ?? 0;
+            let scoreCosts = [-4,-2,-1,0,1,2,3,5,7,10,13,17];
+            let scores = [7,8,9,10,11,12,13,14,15,16,17,18];
+            let stats;
 
-            await interaction.followUp({ content: stats, ephemeral: true});
+            if (maxPoints === 0 || maxPoints < -24 || maxPoints > 102){
+                stats = rollStats();
+                await interaction.followUp({ content: stats, ephemeral: true});
+            } else {
+                stats = randomPointBuy(maxPoints);
+                await interaction.followUp({ content: stats, ephemeral: true});
+            }
+            
+
+            function randomPointBuy(points){ // не работает - добавить ограничители и починить 6й стат - лучше найти новый подход
+                let statsResult = [];
+                let n = 0;
+
+                for (let i = 0 ; i < 5 ; i++){
+                    n = roll(12)-1
+                    points -= scoreCosts[n];
+                    statsResult[i] = scores[n];
+                }
+
+                n = scoreCosts.indexOf(points);
+                statsResult[5] = scores[n];
+                console.log(statsResult);
+
+                return generateOutputMessege(statsResult);
+            }
 
             function rollStats(){
                 let statResult = [0,0,0,0,0,0];
-                let messageResult = '';
 
                 for ( let i = 5 ; i >= 0 ; i-- ){
-
-                    statResult[i] += roll(4,6);
-
+                    statResult[i] += rollDice(4,6);
                 }
 
-                // console.log(statResult);
-
-                statResult.sort(function (a, b) {
-                    return b - a;
-                }
-            );
-            // console.log(statResult);
-
-            for (let i = 0 ; i < 5 ; i++){
-                messageResult += '(' + statResult[i] + ') ';
-            }
-
-
-                return messageResult;
+                return generateOutputMessege(statResult);
 
             }
-            function roll(quantity, dice){
-                let roll = function(dice) {
-                    return Math.floor(Math.random() * dice)+1;
-                }
+            function rollDice(quantity, dice){
                 let result = 0;
                 let diceResults = [0,0,0,0];
 
-                for (let i = 0  ; i < quantity ; i++){
+                do{
 
-                    diceResults[i] += roll(dice)
+                    for (let i = 0  ; i < quantity ; i++){
+                        diceResults[i] = roll(dice)
+                    }
 
-                }
+                    diceResults.sort(function (a, b) {
+                        return b - a;
+                    });
 
-                // console.log('diceResults');
-                // console.log(diceResults);
+                    result = diceResults[0] + diceResults[1] + diceResults[2];
 
-                diceResults.sort(function (a, b) {
-                    return b - a;
-                }
-            );
-                // console.log('diceResultsSorted');
-                // console.log(diceResults);
+                } while (result < 7 || result > 18);
 
-                return result = diceResults[0] + diceResults[1] + diceResults[2];
-
+                return result;
             }
 
+            function roll(dice) {
+                return Math.floor(Math.random() * dice)+1;
+            }
+
+            function generateOutputMessege(statResult){
+                let messageResult = '';
+                let costResult = 0;
+
+                statResult.sort(function (a, b) {
+                    return b - a;
+                });
+
+                for (let i = 0, n = 0 ; i < statResult.length ; i++){
+                    messageResult += '(' + statResult[i] + ') ';
+
+                    n = scores.indexOf(statResult[i]);
+                    costResult += scoreCosts[n];
+                }
+
+                    return messageResult + '\nЦеной по ПБ: ' + costResult;
+            }
         }
 }
