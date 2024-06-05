983d4b872e0f3e481f2facbad0489e87b6030a8b
Void <secretceeper25@gmail.com>
2024-06-05 17:19:14 +0300

    команда /level теперь учитывает прогрессию и слегка оптимизирована

--- commands/utility/level.js
+++ commands/utility/level.js
@@ -27,36 +27,42 @@ module.exports = {
             let input = interaction.options.getInteger('input', true);
             let progression = interaction.options.getString('progression', false) ?? 'medium';
             let result;
+            let prev;
+            let last;  
 
             await interaction.deferReply({ephemeral: true}); //приложение думает
 
             switch (progression){
                 case 'fast':
-                    
+                    prev = 1_700_000;
+                    last = 2_400_000;   
+                    result = calcExpForLevel(input, prev, last);
                     break;
                 case 'medium':
-                    result = calcExpForLevel(input, progression);
+                    prev = 2_550_000;
+                    last = 3_600_000;   
+                    result = calcExpForLevel(input, prev, last);
                     break;
                 case 'slow':
-                    
+                    prev = 3_850_000;
+                    last = 5_350_000;   
+                    result = calcExpForLevel(input, prev, last);
                     break;
             }
 
-            await interaction.followUp({content: "Ввод: `"+ input + "`\nEXP: " + result, ephemeral: true});
+            await interaction.followUp({content: "Ввод: `"+ input + "`\nEXP: " + result + ` Для ${progression} прогрессии`, ephemeral: true});
 
-            function calcExpForLevel(level, progression){
-                let first = 3_600_000;
-                let result;
-                let prev = 2_550_000;
+            function calcExpForLevel(level, prev, last){
+        
+                let result = 0;
+                let difference = last - prev;
                 
                 if(level>20){
                     for(let i=20 ; i<level ; i++){
-
-                        result = (first - prev)*2 + first;
-                        prev = first;
-                        first = result;
-
+                        result += difference*2;
+                        difference *= 2;
                     }
+                    result += last;
                 }
                 return result;
             }
