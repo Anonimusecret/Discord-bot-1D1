61c752afecceaa492e3bf39448b027ea202bc1fc
Void <secretceeper25@gmail.com>
2024-06-20 16:52:35 +0300

    команда /next работает

--- commands/music/next.js
+++ commands/music/next.js
@@ -19,43 +19,18 @@ module.exports = {
             try {
 
                 const queue = useQueue(interaction.guild.id);
-                let success;
 
                 if (!queue?.isPlaying()) return interaction.followUp({ content: `No music currently playing <${interaction.member}>... try again ? <❌>` });
-                
-                //success = queue.node.move(queue.tracks.toArray()[0], queue.tracks.toArray().length)
-                success = queue.node.jump(0)
-                if(success){
-                    return interaction.followUp({ content: 'Skipped!', ephemeral: true});
-                }else{
-                        return interaction.followUp({ content: 'Unexpected error. Try again' + `\n ${queue.tracks.toArray()[0].title}`, ephemeral: true});
-                    }
-                
-                // const unloop = queue.setRepeatMode(QueueRepeatMode.OFF);
-                // if (!unloop){
-                //     const skip = queue.node.skip();
-                //     if (skip){
-                //         const loop = setTimeout(queue.setRepeatMode, 5_000, QueueRepeatMode.TRACK) //queue.setRepeatMode(QueueRepeatMode.TRACK);
-                //         if(!loop){
-                //             return interaction.followUp({ content: 'Skipped!', ephemeral: true});
-                //         }
-                //     }
-                // }
-                
-                // success = queue.node.skip();
-                //TODO: попробовать задержку
-                
-                
 
-                // if (success){
-                //     queue.setRepeatMode(QueueRepeatMode.TRACK);
-                //     return interaction.followUp({ content: 'Skipped!', ephemeral: true});
-                // }else{
-                //     return interaction.followUp({ content: 'Unexpected error. Try again', ephemeral: true});
-                // }
-                
+                queue.setRepeatMode(QueueRepeatMode.OFF)
+                queue.node.skip();
+
+                player.events.once('playerSkip', (queue) => {
+                    queue.setRepeatMode(QueueRepeatMode.TRACK)
+                    return interaction.followUp({ content: 'Skipped!', ephemeral: true});
+                });
+
             } catch (e) {
-                // let's return error if something failed
                 return interaction.followUp(`Something went wrong: ${e}`);
             }
         }
